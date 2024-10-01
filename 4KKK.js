823106abc64646a990aeb45dccf2af1f43fafab9
jvc-eng <70835122+jvc-eng@users.noreply.github.com>
2024-10-01 16:09:33 +0800

    Update 4KKK.js

--- 4KKK.js
+++ 4KKK.js
@@ -3,7 +3,7 @@
 http://111.229.140.167:8762/apptov5/v1/vod/getVod url script-response-body https://raw.githubusercontent.com/Alex0510/Eric/refs/heads/master/surge/Script/wm4k.js
 
 #投屏
-https?:\/\/omts\.tc\.qq\.com\/[^\s]+\.m3u8(\?[^\s]*)? url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/wm4k.js
+https?:\/\/omts\.tc\.qq\.com\/[^\s]+\.m3u8(\?[^\s]*)? url script-request-header https://raw.githubusercontent.com/jvc-eng/Script/main/4Kres.js
 
 [mitm]
 hostname = omts.tc.qq.com, 111.229.140.167:8762
